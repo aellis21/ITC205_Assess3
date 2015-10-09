6eb92d2229cc96bec92dd98d4f44d1db30e8d65e
aellis21 <andr3w3llis@gmail.com>
2015-10-09 12:21:12 +1100

    Loan.java updated
    Signed-off-by: aellis21 <andr3w3llis@gmail.com>

---
+++ src/library/entities/Loan.java
@@ -0,0 +1,104 @@
+package library.entities;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import library.interfaces.entities.ELoanState;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+public class Loan implements ILoan {
+
+	IBook _book;
+	IMember _member;
+	Date _borrowDate, _dueDate;
+	Calendar _calendar;
+	ELoanState _state;
+	int _loanID;
+	
+	
+	public Loan (IBook book, IMember member, Date borrowDate, Date dueDate){
+		if (book == null || 
+				member == null ||
+				borrowDate == null ||
+				dueDate == null){
+			throw new IllegalArgumentException("Parameters cannot be null");
+		}
+		if (dueDate.compareTo(borrowDate) < 0){
+			throw new IllegalArgumentException("Due date before borrow date");
+		}
+		
+		_book = book;
+		_member = member;
+		_borrowDate = borrowDate;
+		_dueDate = dueDate;
+		_state = ELoanState.PENDING;
+	}
+	
+	
+	@Override
+	public void commit(int loanID) {
+		if (loanID <= 0){
+			throw new IllegalArgumentException("Loan ID must be positive integer");
+		}
+		if (_state != ELoanState.PENDING){
+            throw new RuntimeException("Loan 'state' not pending." + _state);
+		}
+		
+        
+        _loanID = loanID;
+        
+        _book.borrow(this);
+        _member.addLoan(this);
+        
+        _state = ELoanState.CURRENT;
+	}
+	
+	@Override
+	public void complete() {
+		if (_state != ELoanState.CURRENT){
+			throw new RuntimeException("Loan state not current");
+		}
+		if(_state != ELoanState.OVERDUE){
+			throw new RuntimeException("Loan state not overdue");
+		}
+		
+		_state = ELoanState.COMPLETE;
+	}
+	
+	@Override
+	public boolean isCurrent() {
+		return _state == ELoanState.CURRENT;
+	}
+
+	@Override
+	public boolean isOverDue() {
+		return _state == ELoanState.OVERDUE;
+	}
+
+	@Override
+	public boolean checkOverDue(Date currentDate) {
+		if (_state != ELoanState.CURRENT || _state != ELoanState.OVERDUE){
+			throw new RuntimeException("Loan state not current or overdue");
+		}
+		
+		return currentDate.after(_dueDate);
+	}
+
+	@Override
+	public IMember getBorrower() {
+		return _member;
+	}
+
+	@Override
+	public IBook getBook() {
+		return _book;
+	}
+
+	@Override
+	public int getID() {
+		return _book.getID();
+	}
+
+}
---
+++ src/test/library/entities/TestLoan.java
@@ -0,0 +1,121 @@
+package test.library.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import library.interfaces.entities.*;
+import library.entities.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+
+
+public class TestLoan {
+	
+	IBook _book;
+	IMember _member;
+	ILoan _loan;
+	Date _borrowDate, _dueDate;
+	Calendar _calendar;
+
+	@Before
+	public void setUp() throws Exception {
+		_book = mock(IBook.class);
+		_member =  mock(IMember.class);
+		
+		_calendar = Calendar.getInstance();
+		_borrowDate = _calendar.getTime();
+		_calendar.add(Calendar.DATE, ILoan.LOAN_PERIOD);
+		_dueDate = _calendar.getTime();
+		
+		_loan = new Loan(_book, _member, _borrowDate, _dueDate);
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_loan = null;
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCreate(){
+		ILoan loan = new Loan(_book, _member, _borrowDate, _dueDate);
+		assertTrue (loan instanceof Loan);
+	}
+		
+	@Test (expected=IllegalArgumentException.class)
+	public void testCreateBadParamBookNull(){
+		ILoan loan = new Loan(null, _member, _borrowDate, _dueDate);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCreateBadParamMemberNull(){
+		ILoan loan = new Loan(_book, null, _borrowDate, _dueDate);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCreateBadParamBorrowDateNull(){
+		ILoan loan = new Loan(_book, _member, null, _dueDate);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCreateBadParamDueDateNull(){
+		ILoan loan = new Loan(_book, _member, _borrowDate, null);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCreateBadParamDueDateLessThanBorrowDate(){
+		ILoan loan = new Loan(_book, _member, _dueDate, _borrowDate);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCommitLoanIDNegative(){
+		_loan.commit(-1);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testCommitLoanIDZero_NoException(){
+		_loan.commit(0);
+	}
+
+	@Test
+	public void isCurrent_StatePending_NoException () {
+		assertFalse(_loan.isCurrent());
+	}
+	
+	@Test
+	public void testComplete() {
+		//_loan.complete();
+	}
+
+	@Test
+	public void testIsOverDue() {
+		assertFalse(_loan.isOverDue());
+	}
+
+	@Test
+	public void testCheckOverDue_OnDue() {
+		assertFalse(_loan.checkOverDue(_dueDate));
+	}
+
+	@Test
+	public void testGetBorrower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+}
